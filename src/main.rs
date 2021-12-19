3cf4d65054f55a5c1bed54e57c354be4bdb6fa1a
= <=>
2021-12-19 18:13:35 -0500

    finally figured it out

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-
 fn print(p: u8) {
     println!("[{:08b}]", p)
 }
 
-fn toggle(n: u8, k: usize) -> u8 {
+fn _toggle(n: u8, k: usize) -> u8 {
     n ^ n << k 
 }
 
@@ -19,21 +18,6 @@ fn clear(n: u8, k: u8) -> u8 {
     n & !(1 << k)
 }
 
-fn set_if(d: u8, mask: u8, b: bool) -> u8 {
-    d ^ (d & !mask) | ((!b as u8) & mask)
-}
-
-fn new_from_str(s: &str) -> u8 {
-    let mut value = 0;
-    match s.chars().count() {
-        2 => { value = encode(&s) }
-        3 => { value = encode(&s) }
-        4 => { value = encode(&s) }
-        8 => { value = encode(&s) }
-        _ => { /* Do nothing */   }
-    }
-    value
-}
 fn encode(s: &str) -> u8 {
     let mut d = 0;
     for c in s.chars() {
@@ -50,30 +34,25 @@ fn encode(s: &str) -> u8 {
     }
     d
 }
+
 fn is_known(d: u8) -> bool {
     get(d, 7) == 1
 }
+
 fn set_known(d: u8) -> u8 {
     set(d, 7) 
 }
+
 fn set_unknown(d: u8) -> u8 {
     clear(d, 7)
-}
-fn compliment(d: u8) -> u8 {
-    set_unknown(!d)
-}
-fn plus(d1: u8, d2: u8) -> u8 {
-    set_unknown(d1 | d2)
-}
-fn intersect(d1: u8, d2: u8) -> u8 {
-    set_unknown(d1 & d2)
-}
-fn diff(d1: u8, d2: u8) -> u8 {
-    set_unknown(d1 & !d2)
 }
 
 fn symdiff(d1: u8, d2: u8) -> u8 {
     set_unknown(d1 ^ d2)
+}
+
+fn diff(d1: u8, d2: u8) -> u8 {
+    set_unknown(d1 & !d2)
 }
 
 fn main() {
@@ -94,91 +73,74 @@ fn main() {
 
     let r: Vec<&str> = str_eg.split(" | ").collect();
     let observed: Vec<&str> = r[0].split_whitespace().collect();
-    let _four_displays: Vec<&str> = r[1].split_whitespace().collect();
-    
-    for s in &observed {
-        let d: u8 = encode(s);
-        print(d);
+    let four_displays: Vec<&str> = r[1].split_whitespace().collect();
+    let outputs = four_displays.clone();
+    let all_str = observed.into_iter().chain(four_displays.into_iter()).collect::<Vec<&str>>();
+
+    //collect knowns
+    for s in &all_str {
         match s.chars().count() {
             2 => { _one   = encode(s);   _one = set_known(_one);  }
-            4 => { _four  = encode(s);  _four = set_known(_four); }
             3 => { _seven = encode(s); _seven = set_known(_seven);}
+            4 => { _four  = encode(s);  _four = set_known(_four); }
             7 => { _eight = encode(s); _eight = set_known(_eight);}
-            6 => {   
-                    if !is_known(_zero) {
-                        _zero = encode(s);
-                        if _zero == plus(diff(diff(_eight.clone(), _one.clone()),_four.clone()), diff(_eight.clone(),_two.clone())) {
-                            _zero = set_known(_zero);
-                            break;
-                        }
-                    }
-                    if !is_known(_six) {
-                        _six = encode(s);
-                        if _six == 99 {}//TODO work out set logic
-                    }
-                    if !is_known(_nine) {
-                        _nine = encode(s);
-                        if _nine == 99 {}// TODO work out set logic
-                    }
-            }
-            5 => {   
-                    if !is_known(_two) {
-                        _two = encode(s);
-                        if _two == 99 {}//TODO work out set logic
-                    }
-                    if !is_known(_five) {
-                        _five = encode(s);
-                        if _five == 99 {}//TODO work out set logic
-                    }
-                    if !is_known(_three) {
-                        _three = encode(s);
-                        if _three == plus(symdiff(_eight, plus(symdiff(_eight,_five),symdiff(_eight,_two))), _one) {
-                            _three = set_known(_three);
-                        }
-                    }
-                }
+            _ => { }
+        }
+    }
+    let _m = |n,m| symdiff(n, m);
+    let _d = |n,m| diff(n, m);
+
+    for s in &all_str {
+        let x = encode(s);
+        let first  = _d(x, _one).count_ones();
+        let second = _d(x, _four).count_ones();
+        let third  = _d(x, _seven).count_ones();
+        let forth  = _m(x, _eight).count_ones();
+        if first == 3 && second == 2 && third == 2 && forth == 2 {
+            _three = x;//done
+            _three = set_known(_three);
+        }
+        if first == 4 && second == 3 && third == 3 && forth == 1 {
+            _zero = x;//done
+            _zero = set_known(_zero);
+        }
+        if first == 4 && second == 3 && third == 3 && forth == 2 {
+            _two = x;//done
+            _two = set_known(_two);
+        }
+        if first == 4 && second == 2 && third == 4 && forth == 2 {
+            _five = x;//done
+            _five = set_known(_five);
+        }
+        if first == 5 && second == 3 && third == 4 && forth == 1 {
+            _six = x;//done
+            _six = set_known(_six);
+        }
+        if first == 4 && second == 2 && third == 3 && forth == 1 {
+            _nine = x;//done
+            _nine = set_known(_nine);
+        }
+    }
+    let mut i = 0;
+    let _ten: u32 = 10;
+    let mut count = 0;
+    for d in &outputs {
+        i += 1;
+        let mut x = encode(d);
+        x = set_known(x);
+        match x {
+            n if _zero  == n => count += 0*_ten.pow(i),
+            n if _one   == n => count += 1*_ten.pow(i),
+            n if _two   == n => count += 2*_ten.pow(i),
+            n if _three == n => count += 3*_ten.pow(i),
+            n if _four  == n => count += 4*_ten.pow(i),
+            n if _five  == n => count += 5*_ten.pow(i),
+            n if _six   == n => count += 6*_ten.pow(i),
+            n if _seven == n => count += 7*_ten.pow(i),
+            n if _eight == n => count += 8*_ten.pow(i),
+            n if _nine  == n => count += 9*_ten.pow(i),
             _ => {}
         }
     }
-    
-    
-    // _zero = set_known(_zero.clone());
-    // let y = union(diff(diff(_eight.clone(), _one.clone()),_four.clone()), diff(_eight.clone(),_two.clone()));
-    // if y != _zero.clone() {
-    //     _zero = set_unknown(_zero.clone());
-    // }
-    // let x = union(diff(_eight.clone(), union(diff(_eight.clone(),_five.clone()),diff(_eight.clone(),_two.clone()))), _one.clone());
-    // if x != _three {
-    //     _three = set_unknown(_three.clone());
-    // }
-
-    if is_known(_zero) {
-        println!("0: ");
-        print(_zero);
-    }
-    if is_known(_three) {
-        println!("3: ");
-        print(_three);
-    }
-    if is_known(_one) {
-        println!("1: ");
-        print(_one);
-    }
-    if is_known(_four) {
-        println!("4: ");
-        print(_four);
-    }
-    if is_known(_seven) {
-        println!("7: ");
-        print(_seven);
-    }
-    if is_known(_eight) {
-        println!("8: ");
-        print(_eight);
-    }
-
-
-    //let mut x = 0b0000_0001;
-    //x = set_known(x);
-    //reminder that I am an idiot
+    println!("Count: {}", count/10);
 }
